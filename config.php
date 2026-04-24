61be082b2df38147bd49e945003730e06fc43b0c
leonarftcraft <leonarftcraft@gmail.com>
2026-04-24 16:13:49 -0400

    Delete config.php

--- config.php
+++
@@ -1,5 +0,0 @@
-<?php
-// Archivo de configuración local - NO SUBIR A GIT
-// Define la llave maestra de encriptación para el sistema
-define('SACRE_ENC_KEY', 'CLAVE_SECRETA_SACREJ_2025_SEGURA');
-?>
